16e701959bce2601a5b158aac8f4205db670d709
yeo_seung_hyeon <00www.black@gmail.com>
2023-10-22 15:48:55 +0900

    init

---
+++ src/main/kotlin/join/famila/FamilaApplication.kt
@@ -0,0 +1,11 @@
+package join.famila
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FamilaApplication
+
+fun main(args: Array<String>) {
+	runApplication<FamilaApplication>(*args)
+}
---
+++ src/test/kotlin/join/famila/FamilaApplicationTests.kt
@@ -0,0 +1,13 @@
+package join.famila
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FamilaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
