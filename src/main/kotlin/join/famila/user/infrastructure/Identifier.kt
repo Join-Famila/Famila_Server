34dc8e7415f98377877f1e77481ebc17a4b44d82
yeo-chi <00www.black@gmail.com>
2023-11-17 13:45:51 +0900

    set 대비 hashCode와 equals override

--- src/main/kotlin/join/famila/user/infrastructure/Identifier.kt
+++ src/main/kotlin/join/famila/user/infrastructure/Identifier.kt
@@ -7,4 +7,16 @@ class Identifier(
     val provider: String,
 
     val uid: String,
-)
+) {
+    override fun hashCode(): Int {
+        return (provider + uid).hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Identifier) {
+            return hashCode() == other.hashCode()
+        }
+
+        return false
+    }
+}
