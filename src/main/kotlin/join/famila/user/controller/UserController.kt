9c2bb9e05ef40f964f7e6399c16968322f6b406a
yeo-chi <00www.black@gmail.com>
2023-12-06 15:47:42 +0900

    user로그인, 회원가입, 토큰 재발급 로직 구현

--- src/main/kotlin/join/famila/user/controller/UserController.kt
+++ src/main/kotlin/join/famila/user/controller/UserController.kt
@@ -1,20 +1,14 @@
 package join.famila.user.controller
 
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletResponse
-import java.math.BigDecimal
 import java.security.Principal
-import java.time.LocalDate
-import join.famila.club.infrastructure.Category
-import join.famila.club.infrastructure.Tag.BIKING
-import join.famila.club.infrastructure.Tag.GOLF
-import join.famila.club.infrastructure.Tag.MEDITATION
-import join.famila.user.controller.data.LocationResponse
+import java.util.UUID.randomUUID
 import join.famila.user.controller.data.SignInRequest
 import join.famila.user.controller.data.SignUpRequest
 import join.famila.user.controller.data.UserResponse
-import join.famila.user.infrastructure.Gender.FEMALE
-import join.famila.user.infrastructure.Gender.MALE
+import join.famila.user.service.UserService
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.HttpStatus.OK
 import org.springframework.web.bind.annotation.GetMapping
@@ -26,33 +20,14 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/v1/users")
-class UserController {
+class UserController(
+    private val userService: UserService,
+) {
     @GetMapping("me")
     @ResponseStatus(OK)
     @Tag(name = "로그인 사용자 정보 가져오는 API", description = "헤더로 전달 받은 Authentication 값을 바탕으로 사용자 정보를 가져옴")
     fun getMe(principal: Principal): UserResponse {
-        return UserResponse(
-            id = 1,
-            name = "테스트이름",
-            profile = "profilePath",
-            gender = FEMALE,
-            phoneNumber = "01012345678",
-            location = LocationResponse(
-                address = "서울특별시 구로구 구로동",
-                latitude = BigDecimal.valueOf(123.4),
-                longitude = BigDecimal.valueOf(233.624),
-            ),
-            birthDay = LocalDate.of(1991, 9, 25),
-            introduce = "안녕하세요. 자기소개 입니다.",
-            categories = setOf(
-                Category(tag = BIKING),
-                Category(tag = GOLF),
-                Category(
-                    tag =
-                    MEDITATION
-                ),
-            ),
-        )
+        return userService.get(id = principal.name.toLong()).let(::UserResponse)
     }
 
     @PostMapping("signIn")
@@ -62,68 +37,53 @@ class UserController {
         @RequestBody signInRequest: SignInRequest,
         httpServletResponse: HttpServletResponse,
     ): UserResponse {
-        httpServletResponse.addHeader(
-            "authentication",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxOlVTRVIiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTUxNjIzOTMyMn0.1JJh--9PzUBQPOzzpSMlhEWs1HuDLq_2NXM5icUgWmo"
-        )
-        httpServletResponse.addHeader(
-            "refreshToken",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIn0.rTCH8cLoGxAm_xw68z-zXVKi9ie6xJn9tnVWjd_9ftE"
-        )
+        httpServletResponse.apply {
+            addHeader(
+                "Authentication",
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxOlVTRVIiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTUxNjIzOTMyMn0.1JJh--9PzUBQPOzzpSMlhEWs1HuDLq_2NXM5icUgWmo"
+            )
+            addCookie(
+                Cookie("identifyCode", randomUUID().toString()).apply { isHttpOnly = true }
+            )
+        }
 
-        return UserResponse(
-            id = 1,
-            name = "테스트이름",
-            profile = "profilePath",
-            gender = MALE,
-            phoneNumber = "01012345678",
-            location = LocationResponse(
-                address = "서울특별시 구로구 구로동",
-                latitude = BigDecimal.valueOf(123.4),
-                longitude = BigDecimal.valueOf(233.624),
-            ),
-            birthDay = LocalDate.of(1991, 9, 25),
-            introduce = "안녕하세요. 자기소개 입니다.",
-            categories = setOf(
-                Category(tag = BIKING),
-                Category(tag = GOLF),
-                Category(tag = MEDITATION),
-            ),
-        )
+        return userService.get(request = signInRequest).let(::UserResponse)
+    }
+
+    @PostMapping("refresh")
+    @ResponseStatus(OK)
+    @Tag(name = "토큰 재발급 API", description = "헤더로 Authentication를 전달하면 토큰이 재발급됨")
+    fun refresh(
+        httpServletResponse: HttpServletResponse,
+    ) {
+        httpServletResponse.apply {
+            addHeader(
+                "Authentication",
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxOlVTRVIiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTUxNjIzOTMyMn0.1JJh--9PzUBQPOzzpSMlhEWs1HuDLq_2NXM5icUgWmo"
+            )
+            addCookie(
+                Cookie("identifyCode", randomUUID().toString()).apply { isHttpOnly = true }
+            )
+        }
     }
 
     @PostMapping
     @ResponseStatus(CREATED)
-    @Tag(name = "회원가입 API", description = "헤더로 Authentication, refreshToken과 사용자 정보를 전달")
+    @Tag(name = "회원가입 API", description = "헤더로 Authentication, refreshToken 과 사용자 정보를 전달")
     fun signUp(
         @RequestBody signUpRequest: SignUpRequest,
         httpServletResponse: HttpServletResponse,
     ): UserResponse {
-        httpServletResponse.addHeader(
-            "authentication",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxOlVTRVIiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTUxNjIzOTMyMn0.1JJh--9PzUBQPOzzpSMlhEWs1HuDLq_2NXM5icUgWmo"
-        )
-        httpServletResponse.addHeader(
-            "refreshToken",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIn0.rTCH8cLoGxAm_xw68z-zXVKi9ie6xJn9tnVWjd_9ftE"
-        )
-
-        return with(signUpRequest) {
-            UserResponse(
-                id = 1,
-                name = name,
-                profile = profile,
-                gender = gender,
-                phoneNumber = phoneNumber,
-                location = LocationResponse(
-                    address = location.address,
-                    latitude = location.latitude,
-                    longitude = location.longitude,
-                ),
-                birthDay = birthDay,
-                introduce = introduce,
-                categories = categories,
+        httpServletResponse.apply {
+            addHeader(
+                "Authentication",
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxOlVTRVIiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTUxNjIzOTMyMn0.1JJh--9PzUBQPOzzpSMlhEWs1HuDLq_2NXM5icUgWmo"
+            )
+            addCookie(
+                Cookie("identifyCode", randomUUID().toString()).apply { isHttpOnly = true }
             )
         }
+
+        return userService.save(user = signUpRequest.toEntity()).let(::UserResponse)
     }
 }
--- src/main/kotlin/join/famila/user/service/UserService.kt
+++ src/main/kotlin/join/famila/user/service/UserService.kt
@@ -1,19 +1,17 @@
 package join.famila.user.service
 
 import join.famila.user.controller.data.SignInRequest
-import join.famila.user.controller.data.SignUpRequest
 import join.famila.user.infrastructure.Identifier
 import join.famila.user.infrastructure.User
 import join.famila.user.infrastructure.UserRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
     private val userRepository: UserRepository,
 ) {
-    fun save(request: SignUpRequest): User {
-        return userRepository.save(request.toEntity())
-    }
+    fun get(id: Long) = userRepository.findByIdOrNull(id = id) ?: throw NoSuchElementException()
 
     fun get(request: SignInRequest): User {
         return userRepository.findByIdentifier(
@@ -22,5 +20,9 @@ class UserService(
                 provider = request.provider,
             )
         ) ?: throw NoSuchElementException()
+    }
+
+    fun save(user: User): User {
+        return userRepository.save(user)
     }
 }
